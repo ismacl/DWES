b636bed1f6f8d3a5ae25a77d3a0a211305c002d5
root <root@ismacl>
2023-11-22 14:02:23 +0100

    Ejercicio 1 sprint4

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -4,12 +4,17 @@
 <html>
 	<body>
 	<?php
+		session_start();
+		$user_id_a_insertar = 'NULL';
+		if (!empty($_SESSION['user_id'])) {
+
+		}
 		$juego_id = $_POST['juego_id'];
 		$comentario = $_POST['new_comment'];
 		$fecha = $_POST("Y-m-d H:i:s");
 
 		$query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id, fecha) 
-VALUES ('".$comentario."',".$juego_id.",NULL, ".$fecha.")";
+VALUES ('".$comentario."',".$juego_id.",".$user_id_a_insertar.")";
 
 	mysqli_query($db, $query) or die('Error');
 
