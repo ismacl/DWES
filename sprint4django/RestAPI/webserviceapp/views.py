b3f53bf7de11cd710c3e9ee231c65e55adf3f353
root <root@ismacl>
2023-11-22 14:32:20 +0100

    Punto 4 sprint4

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def pagina_de_prueba(request):
+	return HttpResponse("<h1>Chimichanga</h1>");
 
 # Create your views here.
